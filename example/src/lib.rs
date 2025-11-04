046abd383674e1c1ea904aeead03749a1b1e2936
awildergoose <61917452+awildergoose@users.noreply.github.com>
2025-11-04 14:43:27 +0200

    feat: draw_text_wrapped

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -8,10 +8,11 @@ use gooseboy::audio::{Audio, AudioInstance};
 use gooseboy::color::Color;
 use gooseboy::framebuffer::{clear_framebuffer, init_fb};
 use gooseboy::input::{get_key, get_mouse_x, get_mouse_y, is_key_just_pressed};
-use gooseboy::keys::{KEY_F, KEY_N};
+use gooseboy::keys::{KEY_BACKSPACE, KEY_ENTER, KEY_F, KEY_GRAVE_ACCENT, KEY_N, KEY_SPACE};
 use gooseboy::{make_audio, text};
 use std::sync::{LazyLock, Mutex};
 
+static mut CONSOLE_INPUT: String = String::new();
 static SOUND: LazyLock<Mutex<Audio>> = make_audio!(test);
 static LAST_SOUND_INSTANCE: LazyLock<Mutex<Option<AudioInstance>>> =
     LazyLock::new(|| Mutex::new(None));
@@ -20,8 +21,6 @@ static LAST_SOUND_INSTANCE: LazyLock<Mutex<Option<AudioInstance>>> =
 fn main() {
     init_fb();
 }
-
-static mut CONSOLE_INPUT: String = String::new();
 
 #[allow(static_mut_refs)]
 #[gooseboy::update]
@@ -34,20 +33,28 @@ fn update(_nano_time: i64) {
     } else if is_key_just_pressed(KEY_N)
         && let Some(ref mut sound) = *last_sound
     {
-        // sound.stop();
-        // *last_sound = None;
         sound.set_pitch(5.0);
     }
 
-    if let Some(key) = get_key()
-        && let Some(character) = char::from_u32(key as u32)
-    {
+    if let Some(key) = get_key() {
         unsafe {
-            CONSOLE_INPUT += &character.to_string();
+            match key {
+                KEY_BACKSPACE => {
+                    CONSOLE_INPUT.pop();
+                }
+                KEY_ENTER => {
+                    CONSOLE_INPUT.push('\n');
+                }
+                KEY_SPACE..=KEY_GRAVE_ACCENT => {
+                    CONSOLE_INPUT.push(key as u8 as char);
+                }
+                _ => {}
+            }
         }
     }
 
     clear_framebuffer(Color::BLACK);
     sprites::ICON_GEAR.blit(get_mouse_x() as usize, get_mouse_y() as usize);
-    text::draw_text(0, 0, &unsafe { CONSOLE_INPUT.clone() }, Color::RED);
+
+    text::draw_text_wrapped(0, 0, &unsafe { CONSOLE_INPUT.clone() }, Color::RED);
 }
--- gooseboy/src/text.rs
+++ gooseboy/src/text.rs
@@ -1,4 +1,8 @@
-use crate::{color::Color, font::FONT, framebuffer::set_pixel};
+use crate::{
+    color::Color,
+    font::FONT,
+    framebuffer::{get_framebuffer_width, set_pixel},
+};
 
 pub fn draw_char(x: usize, y: usize, c: u8, color: Color) {
     for row in 0..8 {
@@ -21,6 +25,32 @@ pub fn draw_text(x: usize, mut y: usize, text: &str, color: Color) {
                 cx = x;
             }
             _ => {
+                draw_char(cx, y, ch, color);
+                cx += 8;
+            }
+        }
+    }
+}
+
+pub fn draw_text_wrapped(x: usize, y: usize, text: &str, color: Color) {
+    draw_text_wrapped_ex(x, y, text, color, get_framebuffer_width())
+}
+
+pub fn draw_text_wrapped_ex(x: usize, mut y: usize, text: &str, color: Color, max_width: usize) {
+    let mut cx = x;
+
+    for ch in text.bytes() {
+        match ch {
+            b'\n' => {
+                y += 8;
+                cx = x;
+            }
+            _ => {
+                if cx + 8 > x + max_width {
+                    cx = x;
+                    y += 8;
+                }
+
                 draw_char(cx, y, ch, color);
                 cx += 8;
             }
