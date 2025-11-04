8ffa1c340d62cb2b4cfdd1721f37e7de27fb68e3
awildergoose <61917452+awildergoose@users.noreply.github.com>
2025-11-04 17:52:34 +0200

    chore: ultraexplain

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,60 +1,38 @@
 #![no_main]
 
-mod sprites {
-    include!("generated/sprites.rs");
-}
+use gooseboy::framebuffer::{get_framebuffer_width, init_fb};
+use gooseboy::text::{draw_text, get_text_width};
+use gooseboy::{color::Color, framebuffer::clear_framebuffer};
 
-use gooseboy::audio::{Audio, AudioInstance};
-use gooseboy::color::Color;
-use gooseboy::framebuffer::{clear_framebuffer, init_fb};
-use gooseboy::input::{get_key, get_mouse_x, get_mouse_y, is_key_just_pressed};
-use gooseboy::keys::{KEY_BACKSPACE, KEY_ENTER, KEY_F, KEY_GRAVE_ACCENT, KEY_N, KEY_SPACE};
-use gooseboy::{make_audio, text};
-use std::sync::{LazyLock, Mutex};
-
-static mut CONSOLE_INPUT: String = String::new();
-static SOUND: LazyLock<Mutex<Audio>> = make_audio!(test);
-static LAST_SOUND_INSTANCE: LazyLock<Mutex<Option<AudioInstance>>> =
-    LazyLock::new(|| Mutex::new(None));
-
+// Every script has to have a main function, make sure to decorate it
+// with gooseboy::main though, or else the script won't start
 #[gooseboy::main]
 fn main() {
+    // Initializes the framebuffer, you are required to initialize this
+    // here if you plan to draw to the screen (which is very likely)
     init_fb();
 }
 
-#[allow(static_mut_refs)]
+// This is also required in every script, the gooseboy::update is required
+// here too, this function runs X times per second where X is equal to your
+// maximum framerate in the options
 #[gooseboy::update]
-fn update(_nano_time: i64) {
-    let mut audio = SOUND.lock().unwrap();
-    let mut last_sound = LAST_SOUND_INSTANCE.lock().unwrap();
-
-    if is_key_just_pressed(KEY_F) {
-        *last_sound = audio.play();
-    } else if is_key_just_pressed(KEY_N)
-        && let Some(ref mut sound) = *last_sound
-    {
-        sound.set_pitch(5.0);
-    }
-
-    if let Some(key) = get_key() {
-        unsafe {
-            match key {
-                KEY_BACKSPACE => {
-                    CONSOLE_INPUT.pop();
-                }
-                KEY_ENTER => {
-                    CONSOLE_INPUT.push('\n');
-                }
-                KEY_SPACE..=KEY_GRAVE_ACCENT => {
-                    CONSOLE_INPUT.push(key as u8 as char);
-                }
-                _ => {}
-            }
-        }
-    }
-
+fn update(nano_time: i64) {
+    // Clear out the screen, erasing everything that was there previously
     clear_framebuffer(Color::BLACK);
-    sprites::ICON_GEAR.blit(get_mouse_x() as usize, get_mouse_y() as usize);
 
-    text::draw_text_wrapped(0, 0, &unsafe { CONSOLE_INPUT.clone() }, Color::RED);
+    // Initialize the string we want to draw to the screen, You can also use Rust's
+    // String type here, with the caveat of having to clone it at draw_text
+    let text = "Hello, world!";
+    // Convert the time from nanoseconds to seconds
+    let time_sec = nano_time as f64 / 1_000_000_000.0;
+    // Get the position of the right corner and subtract the width of the text
+    // to make the text fit into the screen, You can also use draw_text_wrapped
+    // to automatically wrap text if it passes the end of the framebuffer
+    let right_corner = (get_framebuffer_width() - get_text_width(text)) as f64;
+    // Gets us an X position that smoothly moves from the left to the right using sine
+    let x_pos = ((time_sec.sin() * 0.5 + 0.5) * (right_corner - 1.0)) as usize;
+
+    // Finally, draw the text with the red color (or use Color::new(r, g, b, a) or Color::new_opaque(r, g, b))
+    draw_text(x_pos, 0, text, Color::RED);
 }
