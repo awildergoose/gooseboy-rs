dafc323e0bd7109ee63778a241871e74a0b3fcf5
awildergoose <61917452+awildergoose@users.noreply.github.com>
2025-11-05 19:09:31 +0200

    chore: remove makefile and switch to an xtask

---
+++ xtask/src/main.rs
@@ -0,0 +1,50 @@
+use std::{
+    fs,
+    path::Path,
+    process::{Command, exit},
+};
+
+const TARGET: &str = "wasm32-unknown-unknown";
+
+fn main() {
+    build();
+    copy("example.wasm");
+}
+
+fn get_profile() -> String {
+    let is_release = std::env::args().any(|a| a == "--release");
+    (if is_release { "release" } else { "debug" }).to_string()
+}
+
+fn build() {
+    let profile = get_profile();
+    let mut cmd = Command::new("cargo");
+
+    cmd.args(["build", "-p", "example", "--target", TARGET]);
+
+    if profile == "release" {
+        cmd.arg("--release");
+    }
+
+    let status = cmd.status().expect("failed to run cargo build");
+
+    if !status.success() {
+        eprintln!("build failed");
+        std::process::exit(1);
+    }
+}
+
+fn copy(filename: &str) {
+    let profile = get_profile();
+    let src_str = format!("target/{}/{}/{}", TARGET, profile, filename);
+    let src = Path::new(&src_str);
+    let dst = Path::new(&std::env::var("GOOSEBOY_SCRIPTS_FOLDER").expect("the GOOSEBOY_SCRIPTS_FOLDER environment variable is missing! (ex: C:\\Users\\MyUser\\AppData\\Roaming\\.minecraft\\gooseboy\\scripts)")).join(filename);
+
+    if !src.exists() {
+        eprintln!("error: {} not found", src.display());
+        exit(1);
+    }
+
+    fs::create_dir_all(dst.parent().unwrap()).unwrap();
+    fs::copy(src, dst).unwrap();
+}
