037d2b2d5c8990baa2352164ccd2dca3f0b883e6
awildergoose <61917452+awildergoose@users.noreply.github.com>
2025-11-06 13:40:27 +0200

    chore: will the print work

--- doom/src/lib.rs
+++ doom/src/lib.rs
@@ -3,19 +3,29 @@
 use std::ffi::c_char;
 
 use gooseboy::framebuffer::init_fb;
+use gooseboy::log;
 use gooseboy::{color::Color, framebuffer::clear_framebuffer};
 
 type DoomExitFn = extern "C" fn(i32);
+type DoomPrintFn = extern "C" fn(*const c_char);
 
 unsafe extern "C" {
     fn doom_init(argc: i32, argv: *mut *mut c_char, flags: i32);
     fn doom_update();
     fn doom_get_framebuffer(channels: i32) -> *const u8;
     fn doom_set_exit(cb: DoomExitFn);
+    fn doom_set_print(print_fn: DoomPrintFn);
 }
 
 extern "C" fn doom_exit_override(code: i32) {
     panic!("Doom tried to exit with code: {code}");
+}
+
+extern "C" fn doom_print_override(str: *const c_char) {
+    unsafe {
+        let c_str = std::ffi::CStr::from_ptr(str);
+        log!("[DOOM] {}", c_str.to_string_lossy());
+    }
 }
 
 #[gooseboy::main]
@@ -27,6 +37,7 @@ fn main() {
 
     unsafe {
         doom_set_exit(doom_exit_override);
+        doom_set_print(doom_print_override);
         doom_init(0, argv.as_ptr() as *mut *mut c_char, 0);
     }
 }
