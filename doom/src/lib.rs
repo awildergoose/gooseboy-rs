b35d0f4ea2e4937f4a3c7e8d25f01dc04d0fbe1b
awildergoose <61917452+awildergoose@users.noreply.github.com>
2025-11-06 13:36:24 +0200

    rollback(doom, runtime): rollback old log function and last working doom code

--- doom/src/lib.rs
+++ doom/src/lib.rs
@@ -1,91 +1,41 @@
 #![no_main]
 
-use std::ffi::{CStr, CString, c_char};
+use std::ffi::c_char;
 
 use gooseboy::framebuffer::init_fb;
-use gooseboy::{log, mem};
+use gooseboy::{color::Color, framebuffer::clear_framebuffer};
 
 type DoomExitFn = extern "C" fn(i32);
-// type DoomPrintFn = extern "C" fn(*const c_char);
-// type DoomMallocFn = extern "C" fn(i32) -> *mut std::ffi::c_void;
-// type DoomFreeFn = extern "C" fn(*mut std::ffi::c_void);
-// type DoomGetTimeFn = extern "C" fn(*mut i32, *mut i32);
 
 unsafe extern "C" {
-    // fn doom_set_print(cb: DoomPrintFn);
-    fn doom_set_exit(cb: DoomExitFn);
-    // fn doom_set_malloc(cb: DoomMallocFn, cb2: DoomFreeFn);
-    // fn doom_set_gettime(cb: DoomGetTimeFn);
     fn doom_init(argc: i32, argv: *mut *mut c_char, flags: i32);
-    // fn doom_force_update();
-    // fn doom_update();
-    // fn doom_get_framebuffer(channels: i32) -> *const u8;
+    fn doom_update();
+    fn doom_get_framebuffer(channels: i32) -> *const u8;
+    fn doom_set_exit(cb: DoomExitFn);
 }
-
-// extern "C" fn doom_malloc(size: i32) -> *mut std::ffi::c_void {
-//     let ptr = mem::alloc_bytes(size as usize);
-//     ptr as *mut std::ffi::c_void
-// }
-
-// extern "C" fn doom_free(_ptr: *mut std::ffi::c_void) {
-//     // for now, keep this as a stub
-// }
-
-// extern "C" fn doom_print_override(ptr: *const c_char) {
-//     if ptr.is_null() {
-//         return;
-//     }
-
-//     log!("[puredoom] about to start printing a string");
-
-//     let s = unsafe {
-//         let slice = std::slice::from_raw_parts(ptr as *const u8, 1024);
-//         let len = slice.iter().position(|&b| b == 0).unwrap_or(1024);
-//         str::from_utf8(&slice[..len]).unwrap_or("[invalid utf8]")
-//     };
-
-//     log!("[puredoom] {}", s);
-// }
 
 extern "C" fn doom_exit_override(code: i32) {
-    log!("[puredoom] doom requested exit with code: {}", code);
+    panic!("Doom tried to exit with code: {code}");
 }
-
-// extern "C" fn doom_gettime(sec: *mut i32, usec: *mut i32) {
-//     unsafe {
-//         *sec = 0_i32;
-//         *usec = 0_i32;
-//     }
-// }
 
 #[gooseboy::main]
 fn main() {
     init_fb();
 
-    log!("if this fails then something is REALLY wrong");
+    let dummy = std::ptr::null_mut();
+    let argv: [*mut c_char; 1] = [dummy];
 
     unsafe {
         doom_set_exit(doom_exit_override);
-        // doom_set_print(doom_print_override);
-        // trace!(
-        //     doom_set_malloc(doom_malloc, doom_free),
-        //     "set malloc override"
-        // );
-        // trace!(doom_set_gettime(doom_gettime), "set gettime override");
-
-        let arg0 = CString::new("doom.wad").unwrap();
-        let arg0_ptr = arg0.into_raw();
-        let mut argv: [*mut c_char; 1] = [arg0_ptr];
-        doom_init(1, argv.as_mut_ptr(), 0);
-        // doom_force_update();
-
-        let _ = CString::from_raw(arg0_ptr);
+        doom_init(0, argv.as_ptr() as *mut *mut c_char, 0);
     }
 }
 
 #[gooseboy::update]
 fn update(_nano_time: i64) {
-    // unsafe {
-    //     doom_update();
-    // }
+    unsafe {
+        doom_update();
+    }
+
+    clear_framebuffer(Color::BLACK);
 }
--- gooseboy/src/runtime.rs
+++ gooseboy/src/runtime.rs
@@ -1,30 +1,9 @@
-use core::fmt;
-
-pub struct StackWriter<'a> {
-    pub buf: &'a mut [u8],
-    pub pos: usize,
-}
-
-impl<'a> fmt::Write for StackWriter<'a> {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        let bytes = s.as_bytes();
-        let len = bytes.len().min(self.buf.len() - self.pos);
-        self.buf[self.pos..self.pos + len].copy_from_slice(&bytes[..len]);
-        self.pos += len;
-        Ok(())
-    }
-}
-
 #[macro_export]
 macro_rules! log {
     ($($arg:tt)*) => {{
-        use core::fmt::Write;
-        let mut buf = [0u8; 256];
-        let mut writer = $crate::runtime::StackWriter { buf: &mut buf, pos: 0 };
-        let _ = core::write!(&mut writer, $($arg)*);
-
+        let s = format!($($arg)*);
         unsafe {
-            $crate::bindings::log(writer.buf.as_ptr() as i32, writer.pos as i32);
+            $crate::bindings::log(s.as_ptr() as i32, s.len() as i32);
         }
     }};
 }
