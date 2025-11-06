b67bcf2b46cab343eba5a0487da47e7fef87ff0f
awildergoose <61917452+awildergoose@users.noreply.github.com>
2025-11-06 13:25:20 +0200

    chore(doom): remove print override?

--- doom/build.rs
+++ doom/build.rs
@@ -1,12 +1,12 @@
 fn main() {
     println!("cargo:rerun-if-changed=puredoom_wrapper.c");
 
-    cc::Build::new()
-        .include(".")
-        .flag_if_supported("--target=wasm32-unknown-unknown")
-        .flag_if_supported("-nostdinc")
-        .warnings(false)
-        .file("puredoom_wrapper.c")
-        .target("wasm32-unknown-unknown")
-        .compile("puredoom");
+    // cc::Build::new()
+    //     .include(".")
+    //     .flag_if_supported("--target=wasm32-unknown-unknown")
+    //     .flag_if_supported("-nostdinc")
+    //     .warnings(false)
+    //     .file("puredoom_wrapper.c")
+    //     .target("wasm32-unknown-unknown")
+    //     .compile("puredoom");
 }
--- doom/src/lib.rs
+++ doom/src/lib.rs
@@ -6,13 +6,13 @@ use gooseboy::framebuffer::init_fb;
 use gooseboy::{log, mem};
 
 type DoomExitFn = extern "C" fn(i32);
-type DoomPrintFn = extern "C" fn(*const c_char);
+// type DoomPrintFn = extern "C" fn(*const c_char);
 // type DoomMallocFn = extern "C" fn(i32) -> *mut std::ffi::c_void;
 // type DoomFreeFn = extern "C" fn(*mut std::ffi::c_void);
 // type DoomGetTimeFn = extern "C" fn(*mut i32, *mut i32);
 
 unsafe extern "C" {
-    fn doom_set_print(cb: DoomPrintFn);
+    // fn doom_set_print(cb: DoomPrintFn);
     fn doom_set_exit(cb: DoomExitFn);
     // fn doom_set_malloc(cb: DoomMallocFn, cb2: DoomFreeFn);
     // fn doom_set_gettime(cb: DoomGetTimeFn);
@@ -31,21 +31,21 @@ unsafe extern "C" {
 //     // for now, keep this as a stub
 // }
 
-extern "C" fn doom_print_override(ptr: *const c_char) {
-    if ptr.is_null() {
-        return;
-    }
+// extern "C" fn doom_print_override(ptr: *const c_char) {
+//     if ptr.is_null() {
+//         return;
+//     }
 
-    log!("[puredoom] about to start printing a string");
+//     log!("[puredoom] about to start printing a string");
 
-    let s = unsafe {
-        let slice = std::slice::from_raw_parts(ptr as *const u8, 1024);
-        let len = slice.iter().position(|&b| b == 0).unwrap_or(1024);
-        str::from_utf8(&slice[..len]).unwrap_or("[invalid utf8]")
-    };
+//     let s = unsafe {
+//         let slice = std::slice::from_raw_parts(ptr as *const u8, 1024);
+//         let len = slice.iter().position(|&b| b == 0).unwrap_or(1024);
+//         str::from_utf8(&slice[..len]).unwrap_or("[invalid utf8]")
+//     };
 
-    log!("[puredoom] {}", s);
-}
+//     log!("[puredoom] {}", s);
+// }
 
 extern "C" fn doom_exit_override(code: i32) {
     log!("[puredoom] doom requested exit with code: {}", code);
@@ -62,9 +62,11 @@ extern "C" fn doom_exit_override(code: i32) {
 fn main() {
     init_fb();
 
+    log!("if this fails then something is REALLY wrong");
+
     unsafe {
         doom_set_exit(doom_exit_override);
-        doom_set_print(doom_print_override);
+        // doom_set_print(doom_print_override);
         // trace!(
         //     doom_set_malloc(doom_malloc, doom_free),
         //     "set malloc override"
