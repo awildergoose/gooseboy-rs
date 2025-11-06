e90fea35d6c8ec06e04ed493a4a6583b83ca9315
awildergoose <61917452+awildergoose@users.noreply.github.com>
2025-11-06 12:17:40 +0200

    chore(doom): set print override to see what fails

--- doom/src/lib.rs
+++ doom/src/lib.rs
@@ -1,21 +1,34 @@
 #![no_main]
 
-use std::ffi::c_char;
+use std::ffi::{CStr, c_char};
 
 use gooseboy::framebuffer::init_fb;
+use gooseboy::log;
 use gooseboy::{color::Color, framebuffer::clear_framebuffer};
 
 type DoomExitFn = extern "C" fn(i32);
+type DoomPrintFn = extern "C" fn(*const c_char);
 
 unsafe extern "C" {
     fn doom_init(argc: i32, argv: *mut *mut c_char, flags: i32);
     fn doom_update();
     fn doom_get_framebuffer(channels: i32) -> *const u8;
+    fn doom_set_print(print_fn: DoomPrintFn);
     fn doom_set_exit(cb: DoomExitFn);
 }
 
 extern "C" fn doom_exit_override(code: i32) {
     panic!("Doom tried to exit with code: {code}");
+}
+
+extern "C" fn doom_print_override(s: *const c_char) {
+    if s.is_null() {
+        return;
+    }
+    let cstr = unsafe { CStr::from_ptr(s) };
+    if let Ok(msg) = cstr.to_str() {
+        log!("DOOM: {}", msg);
+    }
 }
 
 #[gooseboy::main]
@@ -27,6 +40,7 @@ fn main() {
 
     unsafe {
         doom_set_exit(doom_exit_override);
+        doom_set_print(doom_print_override);
         doom_init(0, argv.as_ptr() as *mut *mut c_char, 0);
     }
 }
