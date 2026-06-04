f836c67123352326a662d337ffa8f6b1b0510ce2
awildergoose <61917452+awildergoose@users.noreply.github.com>
2026-06-04 04:55:43 +0300

    chore: how the hell did i not find out it cant render more than 1 model at once earlier

--- examples/goosegpu/src/lib.rs
+++ examples/goosegpu/src/lib.rs
@@ -1,14 +1,12 @@
 #![no_main]
 
 use gooseboy::{
+    camera::{get_camera_pitch, get_camera_x, get_camera_y, get_camera_yaw, get_camera_z},
     color::Color,
     framebuffer::{clear_framebuffer, init_fb},
-    gpu::{
-        GB_GPU_TEXTURE_ID, GpuCommand, GpuCommandBuffer, PrimitiveType, Vertex, defer_gpu,
-        gpu_read_value,
-    },
-    input::grab_mouse,
-    log,
+    gpu::{GpuCommand, GpuCommandBuffer, PrimitiveType, gpu_read_value, load_obj},
+    input::{grab_mouse, is_key_down},
+    keys::KEY_F,
     system::convert_nano_time_to_seconds,
     text::draw_text_formatted,
     vek::Wrap,
@@ -27,27 +25,19 @@ fn main() {
 fn gpu_main() {
     grab_mouse();
 
-    let quad_vertices = [
-        Vertex::new(-0.5, -0.5, 0.0, 0.0, 0.0),
-        Vertex::new(0.5, -0.5, 0.0, 1.0, 0.0),
-        Vertex::new(0.5, 0.5, 0.0, 1.0, 1.0),
-        Vertex::new(-0.5, 0.5, 0.0, 0.0, 1.0),
-    ];
-
-    let mut buffer = GpuCommandBuffer::new();
-    buffer.insert_register_sprite(&sprites::ICON);
-    buffer.insert(&GpuCommand::PushRecord(PrimitiveType::Quads));
-    buffer.insert(&GpuCommand::BindTexture(0));
-    buffer.insert(&GpuCommand::EmitVertices(Box::new(quad_vertices)));
-    buffer.insert(&GpuCommand::PopRecord);
-    let _ = buffer.upload();
-
+    let vertices = load_obj(include_str!("../teapot.obj"), true);
+    let vertices2 = load_obj(include_str!("../teapot.obj"), true);
     let mut buffer = GpuCommandBuffer::new();
     buffer.insert_register_sprite(&sprites::CAT);
+    buffer.insert(&GpuCommand::PushRecord(PrimitiveType::Triangles));
+    buffer.insert(&GpuCommand::BindTexture(0));
+    buffer.insert(&GpuCommand::EmitVertices(vertices.into()));
+    buffer.insert(&GpuCommand::PopRecord);
+    buffer.insert(&GpuCommand::PushRecord(PrimitiveType::Triangles));
+    buffer.insert(&GpuCommand::BindTexture(0));
+    buffer.insert(&GpuCommand::EmitVertices(vertices2.into()));
+    buffer.insert(&GpuCommand::PopRecord);
     let _ = buffer.upload();
-    defer_gpu();
-    let tx = gpu_read_value::<u32>(GB_GPU_TEXTURE_ID);
-    log!("got texture id!! {tx}");
 }
 
 #[gooseboy::update]
@@ -63,10 +53,15 @@ fn update(nano_time: i64) {
         0,
         0,
         format!(
-            "status: {:#?}\nlast record: {:#?}\nlast texture: {:#?}\nangle: {angle}",
+            "status: {:#?}\nlast record: {:#?}\nlast texture: {:#?}\nangle: {angle}\nrot: {} {}\npos: {} {} {} ",
             gpu_read_value::<u32>(gooseboy::gpu::GB_GPU_STATUS),
             gpu_read_value::<u32>(gooseboy::gpu::GB_GPU_RECORD_ID),
-            gpu_read_value::<u32>(gooseboy::gpu::GB_GPU_TEXTURE_ID)
+            gpu_read_value::<u32>(gooseboy::gpu::GB_GPU_TEXTURE_ID),
+            get_camera_yaw(),
+            get_camera_pitch(),
+            get_camera_x(),
+            get_camera_y(),
+            get_camera_z()
         ),
         Color::RED,
     );
@@ -75,12 +70,29 @@ fn update(nano_time: i64) {
 
     buffer.insert(&GpuCommand::Push);
     buffer.insert(&GpuCommand::RotateAxis {
-        x: 0.0,
-        y: 1.0,
+        x: 1.0,
+        y: 0.0,
         z: 0.0,
-        angle,
+        angle: (180.0_f32).to_radians(),
     });
+    // buffer.insert(&GpuCommand::Translate {
+    //     x: 0.0,
+    //     y: 320.0,
+    //     z: 0.0,
+    // });
     buffer.insert(&GpuCommand::DrawRecorded(0));
     buffer.insert(&GpuCommand::Pop);
+
+    if is_key_down(KEY_F) {
+        buffer.insert(&GpuCommand::Push);
+        buffer.insert(&GpuCommand::Translate {
+            x: 30.0,
+            y: 0.0,
+            z: 0.0,
+        });
+        buffer.insert(&GpuCommand::DrawRecorded(1));
+        buffer.insert(&GpuCommand::Pop);
+    }
+
     let _ = buffer.upload();
 }
